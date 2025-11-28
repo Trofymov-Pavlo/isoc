d55ea6cc9dd9e6bf18b94e51b5ba18d6d51fcf3b
Pashtet1337 <pavel.t2003@gmail.com>
2025-11-28 16:17:40 +0100

    feat guide

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,18 @@
 variant="tonal"
 color="red"
 ></v-card>
+
+<Pere/>
+
+
 </template>
 
 
 <script lang="ts">
 
+import Pere from '~/components/Pere.vue';
+
 </script>
 
 <style lang="scss" scoped>
-.gg{
-  }
 </style>
