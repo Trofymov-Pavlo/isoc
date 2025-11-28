95beb8420151151ba46d365bbac059e9b34e2e62
Pashtet1337 <pavel.t2003@gmail.com>
2025-11-28 15:41:18 +0100

    fix old shit;

---
+++ pages/main.vue
@@ -0,0 +1,16 @@
+<template>
+<v-card  class="gg" text="asdaskdfjasl kdalsdj lakjsd "
+variant="tonal"
+color="red"
+></v-card>
+</template>
+
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.gg{
+  }
+</style>
