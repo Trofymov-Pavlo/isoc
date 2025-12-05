e21782b63fc1679ccc61ab0d9557cce2a7a17f2a
darnak74 <antoineten69@gmail.com>
2025-12-05 15:43:52 +0100

    feat(layout): render MainHeader and SubHeader globally

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,12 @@
 <template>
+  <MainHeader />
+  <SubHeader />
   <slot />
 </template>
+
+<script setup lang="ts">
+import MainHeader from '@/components/header/mainHeader.vue'
+import SubHeader from '@/components/header/sub_header.vue'
+</script>
 
 
