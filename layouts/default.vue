dd595aba9d9661dc481d2f076f4d6ca6c913ef64
darnak74 <antoineten69@gmail.com>
2025-12-05 19:47:05 +0100

    fix: remove duplicate SubHeader from layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,10 @@
 <template>
   <MainHeader />
-  <SubHeader />
   <slot />
 </template>
 
 <script setup lang="ts">
-import MainHeader from '@/components/header/mainHeader.vue'
-import SubHeader from '@/components/header/sub_header.vue'
+
 </script>
 
 
