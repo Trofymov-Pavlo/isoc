6419b92c9f9cbffbf2463c70cf1bd4a4f0288199
darnak74 <antoineten69@gmail.com>
2025-12-05 20:12:06 +0100

    feat: create enDirect page route

---
+++ pages/enDirect.vue
@@ -0,0 +1,13 @@
+<template>
+  <MainHeader />
+  <SubHeader />
+  <EnDirectPage />
+</template>
+
+<script setup lang="ts">
+import MainHeader from '@/components/header/mainHeader.vue';
+import SubHeader from '@/components/header/sub_header.vue';
+import EnDirectPage from '~/components/enDirect/enDirectPage.vue';
+</script>
+
+<style scoped></style>
