45ad4898b907f5acea3bb1a22893bd084b0c173b
darnak74 <antoineten69@gmail.com>
2025-12-05 22:39:41 +0100

    feat: Added 18px margins to the left and right sides

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,17 @@
 <template>
-  <MainHeader />
-  <slot />
+  <div class="site-container">
+    <MainHeader />
+    <slot />
+  </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
-
+<style>
+.site-container {
+  max-width: 1300px;   /* largeur du site comme Mediapart */
+  margin: 0 auto;      /* centre le contenu */
+  padding: 0 18;     /* marges gauche et droite */
+}
+</style>
