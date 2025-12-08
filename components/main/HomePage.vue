248a5982957ad5be84887a5a314fb426d211dbef
darnak74 <antoineten69@gmail.com>
2025-12-08 19:53:39 +0100

    feat: create HomePage parent component orchestrating all home sections

---
+++ components/main/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeroSection />
+    <BreakingNews />
+    <FeaturedArticle />
+    <TopStories />
+    <LiveUpdates />
+    <LatestArticles />
+    <AnalysisSection />
+    <TopicsSection />
+    <NewsletterSection />
+  </div>
+</template>
+
+<script setup lang="ts">
+import HeroSection from './HeroSection.vue';
+import BreakingNews from './BreakingNews.vue';
+import FeaturedArticle from './FeaturedArticle.vue';
+import TopStories from './TopStories.vue';
+import LiveUpdates from './LiveUpdates.vue';
+import LatestArticles from './LatestArticles.vue';
+import AnalysisSection from './AnalysisSection.vue';
+import TopicsSection from './TopicsSection.vue';
+import NewsletterSection from './NewsletterSection.vue';
+</script>
+
+<style scoped>
+.home {
+  background: #ffffff;
+  min-height: 100vh;
+  padding-bottom: 60px;
+}
+</style>
