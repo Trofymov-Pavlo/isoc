dce998472466492150d53148c82a55a0ad642756
darnak74 <antoineten69@gmail.com>
2025-12-14 11:28:57 +0100

    Create video page component

---
+++ Front/pages/video.vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <video-page />
+  </main>
+</template>
+
+<script setup lang="ts">
+import videoPage from '~/components/video/videoPage.vue';
+</script>
+
+<style lang="scss" scoped>
+</style>
