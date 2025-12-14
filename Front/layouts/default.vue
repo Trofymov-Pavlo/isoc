154c018af7b018ccd01e9d40e4208a88358840b9
darnak74 <antoineten69@gmail.com>
2025-12-14 20:06:42 +0100

    Corrige le layout avec HeaderSubHeader pour afficher le subheader

--- Front/layouts/default.vue
+++ Front/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="site-container">
-    <HeaderHeaderMain />
+    <HeaderMain />
   </div>
 
   <HeaderSubHeader />
@@ -9,7 +9,7 @@
     <slot />
   </div>
 
-  <FooterFooterMain />
+  <FooterMain />
 </template>
 
 <style>
