ad6ad443da7e65ae7262f8d5042782f05f280a57
darnak74 <antoineten69@gmail.com>
2025-12-14 11:27:06 +0100

    Link featured article to featuredArticle page

--- Front/components/HomePage/FeaturedArticle.vue
+++ Front/components/HomePage/FeaturedArticle.vue
@@ -13,7 +13,7 @@
             <span class="featured-author">Par Antoine TENA</span>
             <span class="featured-date">Il y a 2h</span>
           </div>
-          <a href="#" class="read-more">Lire l'article complet →</a>
+          <NuxtLink to="/featuredArticle" class="read-more">Lire l'article complet →</NuxtLink>
         </div>
       </article>
     </div>
