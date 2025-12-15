1296c26b2fb14149bf7c33193e136382dd75ce2f
darnak74 <antoineten69@gmail.com>
2025-12-15 17:36:54 +0100

    chore: Supprimer tout le support/soutien et PayPal\n\n- Supprimer page front support et boutons\n- Retirer endpoints et module payments (PayPal)\n- Nettoyer settings et .env des variables PayPal/Stripe\n- Retirer checkout Stripe/PayPal des vues donations\n- Retirer dépendances Stripe/PayPal

--- Back/isoc_auth/urls.py
+++ Back/isoc_auth/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/donations/', include('donations.urls')),
-    path('api/payments/', include('payments.urls')),
 ]
