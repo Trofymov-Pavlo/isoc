07f08534f720ce9dd33ac07ab3668fe01c4818aa
darnak74 <antoineten69@gmail.com>
2025-12-15 17:41:21 +0100

    chore: Remove donations app, payments module, support pages/components and header button; purge support references

--- Back/isoc_auth/urls.py
+++ Back/isoc_auth/urls.py
@@ -7,5 +7,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('api/donations/', include('donations.urls')),
 ]
